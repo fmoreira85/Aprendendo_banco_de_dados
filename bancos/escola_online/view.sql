8fc064bdfbe0e9afad720e39d6f0ef9064dcfe1a
fmoreira85 <fabiomoreiradacunha1@gmail.com>
2026-05-06 10:49:29 -0400

    INNER JOIN

--- bancos/escola_online/view.sql
+++ bancos/escola_online/view.sql
@@ -6,3 +6,18 @@ SELECT * FROM alunos;
 SELECT nome_curso,categoria,preco
 FROM cursos
 WHERE PRECO > 250;
+
+-- Buscar alunos de São Paulo
+SELECT nome,cidade
+FROM alunos
+WHERE cidade = 'são paulo'
+
+-- 4.Listar matrículas com nome do aluno e curso
+
+SELECT 
+    alunos.nome,
+    cursos.nome_curso,
+    matriculas.status_matricula
+FROM matriculas
+INNER JOIN alunos ON matriculas.id_aluno = alunos.id_aluno
+INNER JOIN cursos ON matriculas.id_curso = cursos.id_curso;
